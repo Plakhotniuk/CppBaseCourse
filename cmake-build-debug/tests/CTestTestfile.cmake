5b81c9e76ee5eb1c8496fbc44178330ab359483d
Arseniy <plakhotniuk.av@phystech.edu>
2024-09-15 19:22:20 +0300

    init

---
+++ cmake-build-debug/tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/arseniy/study/Магистратура/1 семестр/CppBase/repo/CppBaseCourse/tests
+# Build directory: /Users/arseniy/study/Магистратура/1 семестр/CppBase/repo/CppBaseCourse/cmake-build-debug/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
