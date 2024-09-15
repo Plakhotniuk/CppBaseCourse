85d8a1f0174b375a2cac1960a2fdd6d64c8cf701
Arseniy <plakhotniuk.av@phystech.edu>
2024-09-15 19:36:43 +0300

    del

--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/arseniy/study/Магистратура/1 семестр/CppBase/repo/CppBaseCourse/tests
-# Build directory: /Users/arseniy/study/Магистратура/1 семестр/CppBase/repo/CppBaseCourse/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(run_test_cache "/Users/arseniy/study/Магистратура/1 семестр/CppBase/repo/CppBaseCourse/cmake-build-debug/tests/run_test_cache")
-set_tests_properties(run_test_cache PROPERTIES  _BACKTRACE_TRIPLES "/Users/arseniy/study/Магистратура/1 семестр/CppBase/repo/CppBaseCourse/tests/CMakeLists.txt;14;add_test;/Users/arseniy/study/Магистратура/1 семестр/CppBase/repo/CppBaseCourse/tests/CMakeLists.txt;0;")
