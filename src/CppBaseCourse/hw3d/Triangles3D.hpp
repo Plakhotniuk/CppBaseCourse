bb1c3e9eefee0cf9c4ae6fbcd37fc3dd334da79b
Plakhotniuk <plakhotniuk.av@phystech.edu>
2024-11-01 21:38:54 +0300

    hw3d started

--- src/CppBaseCourse/hw3d/Triangles3D.hpp
+++ src/CppBaseCourse/hw3d/Triangles3D.hpp
@@ -1,33 +1,73 @@
 #pragma once
+#include "Primitives3D.hpp"
+#include <optional>
 #include <array>
-#include <cassert>
-#include <cmath>
-#include <iostream>
-#include <vector>
 
-namespace triangles_3d {
+namespace triangles3D {
 
-struct point_t {
-  double x = NAN, y = NAN, z = NAN;
+using namespace primitives3D;
 
-  void print() const { std::cout << "(" << x << " ; " << y << " ; " << z << ")"; }
 
-  bool valid() const { return !(x!=x || y!=y || z!=z); }
+struct Triangle3D {
 
-  bool equal(const point_t& rhs) const
-  {
-      assert(valid() && rhs.valid());
-      return (std::abs(x - rhs.x)<std::numeric_limits<double>::epsilon()) &&
-             (std::abs(y - rhs.y)<std::numeric_limits<double>::epsilon()) &&
-             (std::abs(z - rhs.z)<std::numeric_limits<double>::epsilon());
+  std::array<Vec3, 3> points_;
+
+  Triangle3D(const Vec3& vec1, const Vec3& vec2, const Vec3& vec3): points_{vec1, vec2, vec3} {};
+
+  std::array<Vec3, 2> getTwoOtherPoints(size_t num) const {
+    return {points_[(num + 1) % 3], points_[(num + 2) % 3]};
   }
+
+  Plane constructPlane(size_t thirdPoint) const {
+    const Vec3 A = points_[(thirdPoint + 1) % 3];
+    const Vec3 B = points_[(thirdPoint + 2) % 3];
+    const Vec3 C = points_[thirdPoint];
+    const Vec3 AB = B - A;
+    const Vec3 AC = C - A;
+    const Vec3 AH = AB.normalize() * AB.dot(AC) / AB.norm();
+    Vec3 planeNormVec = (AC - AH).normalize();
+
+    return Plane(planeNormVec, A);
+  }
+
 };
 
-struct line_t{
-  point_t p1, p2;
+bool is_intersect(const Triangle3D& triangle1, const Triangle3D& triangle2) {
+  // for point in thisTriangle.points
+  for(size_t i = 0; i < 3; ++i) 
+  {
+    const Plane plane = triangle1.constructPlane(i);
+    const PlaneRelPosition thisPointPos = plane.pointPosition(triangle1.points_[i]);
 
-};
+    // for otherPoint in otherTriangle.points
+    bool hasPointInHalfSpace = false;
+    for(size_t j = 0; j < 3; ++j)
+    {
+      const PlaneRelPosition otherPointPos = plane.pointPosition(triangle2.points_[j]);
+      if(otherPointPos == thisPointPos)
+      {
+        hasPointInHalfSpace = true;
+        break;
+      }
+    }
+    if(!hasPointInHalfSpace) return false;
+  }
+  return true;
+}
 
+size_t countIntersections(const std::vector<Triangle3D>& triangles)
+{
+  size_t counter = 0;
+  const size_t vecSize = triangles.size();
+  for(size_t i = 0; i < vecSize - 1; ++i)
+  {
+    for(size_t j = i + 1; j < vecSize; ++j)
+    {
+      if(is_intersect(triangles[i], triangles[j])) ++counter;
+    }
+  }
+  return counter;
+}
 
 }
 
